e93e8d3f2993096dd6de459f6a32d29b0447dcb6
changsun <changrybirds@users.noreply.github.com>
2018-01-09 11:37:54 -0800

    Add timestamp to schema; set up first db migration script

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class Tracker(db.Model):
 	
@@ -6,6 +7,7 @@ class Tracker(db.Model):
 	phone_number = db.Column(db.String(24), index=True)
 	vendor = db.Column(db.String(64), index=True)
 	url = db.Column(db.String(512))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 	def __repr__(self):
 		return '<Tracker # {}>'.format(self.id)
