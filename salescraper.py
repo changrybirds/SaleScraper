d840633d044ebd22c405e9e661f1a2f9dbdacbb4
changsun <changrybirds@users.noreply.github.com>
2018-01-09 12:12:59 -0800

    Add db; update shell context processor

--- salescraper.py
+++ salescraper.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import Tracker
+
+# save time importing database instance and models to shell sessions
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'Tracker': Tracker}
