f4d89c0550c5b4582564fa380c0304dc54ac519d
changsun <changrybirds@users.noreply.github.com>
2018-01-17 22:13:27 -0800

    Delete records older than 1 year; remove return statements

--- app/dbquery.py
+++ app/dbquery.py
@@ -1,11 +1,17 @@
 from app import app, db, scraper
 from app.models import Tracker
+from datetime import datetime
 
 def dbQuery():
 	trackers = Tracker.query.all()
 	for t in trackers:
 		sale_status = scraper.scrape_sale_status(t.vendor, t.url)
 		if sale_status:
-			return True # insert correct text
+			print(True) # insert correct text
 		else:
-			return False # insert correct text
+			time_diff = datetime.utcnow - t.timestamp
+			if time_diff.total_seconds() > 31557600:
+				db.session.delete(t)
+				db.session.commit()
+			else:
+				print(False) # insert correct text
